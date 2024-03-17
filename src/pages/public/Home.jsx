398a4328b7fbb9e698b39f6fe8af332ecd1fa531
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-17 19:11:13 +0600

    add overview section

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,20 +1,39 @@
-import { Button } from "keep-react";
-import Swal from "sweetalert2";
+import { Tabs } from "keep-react";
+import overviewVideo from "@/assets/overview-video.mp4";
 
 const Home = () => {
-  const handleClick = () => {
-    Swal.fire({
-      icon: "success",
-      title: "this is title",
-      showCancelButton: true,
-      cancelButtonText: "Cancel",
-      confirmButtonText: "Confirm",
-    });
-  };
   return (
-    <div>
-      <Button onClick={handleClick}>Click</Button>
-    </div>
+    <>
+      <h3 className="text-center text-3xl font-bold">
+        Tourism and Travel Guide
+      </h3>
+      <Tabs
+        className="my-10 flex flex-col items-center justify-center"
+        activeLabel="overview"
+      >
+        <Tabs.List>
+          <Tabs.Item label="overview">Overview</Tabs.Item>
+          <Tabs.Item label="packages">Our Packages</Tabs.Item>
+          <Tabs.Item label="tourGuide">Meet Our Tour Guides</Tabs.Item>
+        </Tabs.List>
+        <div>
+          <Tabs.Content className="relative" label="overview">
+            <video width={640} height={360} autoPlay loop>
+              <source src={overviewVideo} type="video/mp4" />
+            </video>
+            <div className="absolute inset-0 z-50 flex items-end bg-gradient-to-b from-transparent to-gray-900/80 p-2 text-gray-50">
+              <div className="text-3xl font-bold">
+                We are offering the best <br /> touring guide that you ever
+                <br />
+                experience
+              </div>
+            </div>
+          </Tabs.Content>
+          <Tabs.Content label="packages">Packages</Tabs.Content>
+          <Tabs.Content label="tourGuide">Meet Our Tour Guides</Tabs.Content>
+        </div>
+      </Tabs>
+    </>
   );
 };
 
