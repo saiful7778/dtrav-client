26b051ca5e9342afc3d1056eb064bf0dcd48fcbd
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-18 11:55:53 +0600

    add tenstack query

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import "./assets/styles/style.css";
 import { RouterProvider } from "react-router-dom";
 import { route } from "@/routes";
 import AuthContextProvider from "./contexts/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <RouterProvider router={route} />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <RouterProvider router={route} />
+      </AuthContextProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
